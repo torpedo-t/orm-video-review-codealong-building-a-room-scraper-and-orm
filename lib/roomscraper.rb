bb185a0c90f1f14fb5f7813e427366dd5c43f670
torpedo-t <55634516+torpedo-t@users.noreply.github.com>
2020-10-15 11:06:30 -0500

    Done.

--- lib/roomscraper.rb
+++ lib/roomscraper.rb
@@ -1,3 +1,13 @@
 class RoomScraper
 
+    def initialize(index_url)
+        @doc = Nokogiri::HTML(open(index_url))
+        binding.pry
+    end
+
+    def rows
+        # this line of code is saying that, if @rows already exists leave it be
+        # if it does not exist, assign it to what's on the left of our #or= method
+        @rows ||= @doc.search("div.content span.rows p.row")
+    end
 end
